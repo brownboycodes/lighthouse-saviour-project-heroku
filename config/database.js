f06e3b78bda32f68f1e7d3fbe088060f5b479912
brownboycodes <ideatorrraj@gmail.com>
2021-07-05 20:57:56 +0530

    changes made to configuration files

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-/*
+
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
@@ -8,9 +8,9 @@ const db = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.e
         useRTC: false
     },
     timezone: '+05:30'
-});*/
+});
 
-const db = new Sequelize(process.env.DATABASE_URL);
+// const db = new Sequelize(process.env.DATABASE_URL);
 
 module.exports = db;
 
