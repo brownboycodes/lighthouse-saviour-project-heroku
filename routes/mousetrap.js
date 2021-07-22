56f9c70bf0a3806f8dc408f3ebf78e74656be50b
brownboycodes <ideatorrraj@gmail.com>
2021-07-22 16:29:16 +0530

    removed errors

--- routes/mousetrap.js
+++ routes/mousetrap.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
-// let bodyParser = require("body-parser");
-//TODO: create application/json parser
-// let jsonParser = bodyParser.json;
-const db = require("../config/database");
+
 // const Mousetrap = require('../models/Mousetrap');
 
 //*getting individual tables
